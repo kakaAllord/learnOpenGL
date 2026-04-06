15aacb9563d1317a228741990a3f1a5b650eb0f0
kakaAllord <allordcodes@gmail.com>
2026-04-06 23:33:53 +0300

    Hello learnOpenGL, say hi to the core profile way, VAO and Shaders are here. Should we do some abstraction?

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,15 @@ int main() {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
-    //
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /*
+     *We are now enabling the core profile, meaning that we have to specify our
+     *own shaders and Vertex Array Object
+     *
+     */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -58,11 +63,85 @@ int main() {
     //      will only see that portion of the window we actually created.
     glViewport(0, 0, 800, 600);
 
+
+    //A shader is just a program that runs in the GPU
+
+    /*
+     *The Vertex shader is responsible with handling the position of vertices.
+     */
+    const char* VertexShadersource =
+        "#version 330 core\n"
+        "\n"
+        "in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "gl_Position = vec4(aPos, 1.0f);"
+        "}\n";
+
+    /*
+     *The Fragment Shader is responsible with handing the color stuff
+     */
+    const char* FragmentShaderSource =
+        "#version 330 core\n"
+        "\n"
+        "out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "color = vec4(1.0f, 0.0f, 0.0f, 1.0f);"
+        "}\n";
+
+    // Compile the shader so we can link it and create a Shader Program later
+    unsigned int VertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VertexShader, 1, &VertexShadersource, nullptr);
+    glCompileShader(VertexShader);
+
+    // Checking for compile status
+    int vertex_compiled;
+    glGetShaderiv(VertexShader, GL_COMPILE_STATUS, &vertex_compiled);
+
+    // Printing error in case of one in the shader
+    if (!vertex_compiled) {
+        char message[1024];
+        glGetShaderInfoLog(VertexShader, 1024, nullptr, message);
+        std::cerr << "Faile to compile Vertex Shader: " << message << std::endl;
+    }
+
+    // Compiling the FragmentShader
+    unsigned int FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FragmentShader, 1, &FragmentShaderSource, nullptr);
+    glCompileShader(FragmentShader);
+
+    //Fragment Shader Error Handling
+    int fragment_compiled;
+    glGetShaderiv(FragmentShader, GL_COMPILE_STATUS, &fragment_compiled);
+    if (!fragment_compiled) {
+        char message[1024];
+        glGetShaderInfoLog(FragmentShader, 1024, nullptr, message);
+        std::cerr << "Failed to compile Fragment Shader : " << message << std::endl;
+    }
+
+    // He we create a program then link the shaders/
+    unsigned int ShaderProgram = glCreateProgram();
+    glAttachShader(ShaderProgram, VertexShader);
+    glAttachShader(ShaderProgram, FragmentShader);
+
+    glLinkProgram(ShaderProgram);
+    glValidateProgram(ShaderProgram);
+
     float positions[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f,  -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
     };
+
+    /*
+     *The Vertex Array Object handles the Array Attributes of the
+     *current bound VBO
+     *
+     */
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
@@ -73,12 +152,17 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //code here
+        // We then bind our shaderProgram and the Vertex Array before drawing our triangles
+        glUseProgram(ShaderProgram);
+        glBindVertexArray(VAO);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
